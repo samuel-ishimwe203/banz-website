689d463d94e1690b79d65f1749fe6c5ef6d7d6f2
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-20 00:35:13 +0200

    new changes

--- src/components/Sixth6.jsx
+++ src/components/Sixth6.jsx
@@ -15,7 +15,71 @@ function Sixth6() {
            </div>
 
     </div>
-    <hr className='bg-white'/>
+    <hr />
+
+    <div className='flex gap-17 p-30 mt-20'>
+        
+        <div><p className='text-white font-bold text-2xl pt-3'>More Information</p></div>
+
+        <div className='text-white'> 
+               <p className='font-bold'>Company</p>
+                <div className='text-gray-400'>
+               <p className='mt-5'><a href="#">Overview</a></p>
+                <p><a href="#">Careers</a></p>
+                 <p><a href="#">Inventors</a></p>
+                  <p><a href="#">Media site</a></p>
+                   <p><a href="#">Overview</a></p>
+                   </div>
+        </div>
+
+         <div className='text-white'> 
+               <p className='font-bold'>Knowledge</p>
+                    <div className='text-gray-400'>
+               <p className='mt-5'><a href="#">Detacted to detail</a></p>
+                <p><a href="#">Our History</a></p>
+                 <p><a href="#">Inventors</a></p>
+                  <p><a href="#">Real Driving Emissions</a></p>
+                  </div>
+                   
+        </div>
+
+
+         <div className='text-white'> 
+               <p className='font-bold'>Business Services</p>
+               <div className='text-gray-400'>
+               <p className='mt-5'><a href="#">Fleet Sales</a></p>
+                <p><a href="#">International Diplomatic sales</a></p>
+                 <p><a href="#">Mercedes-Benz GUARD</a></p>
+                  <p><a href="#">Training for Authorized Dealers</a></p>
+                  </div>
+                   
+        </div>
+        
+        </div>
+
+        <hr />
+
+
+        <div className='text-gray-400 p-20 '>
+            <p>1
+The stated values were determined in accordance with the prescribed WLTP 
+(Worldwide harmonised Light vehicles Test Procedure) measurement procedure. 
+The ranges given refer to the German market. The fuel consumption, energy 
+consumption and CO2 emissions of a car depend not only on the efficient use 
+of the fuel or energy source by the car, but also on driving style and other non-technical factors.</p>
+        </div>
+
+
+        <div>
+               <p><a href="#">Provider</a></p>
+               <p><a href="#">Legal Notice</a></p>
+               <p><a href="#">Settings</a></p>
+               <p><a href="#">Privacy</a></p>
+               <p><a href="#">Contact</a></p>
+               <p><a href="#">Third Party Licence Notices</a></p>
+               <p><a href="#">Don't Sales My Personal Information(CCPA)</a></p>
+               <p><a href="#">Accessibility</a></p>
+        </div>
     </div>
   )
 }
